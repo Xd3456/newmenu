920d9d08605aecf2be8b421b4bb7cc51f7d65d48
Xd3456 <115712144+Xd3456@users.noreply.github.com>
2025-02-09 02:10:32 +0800

    Update ThemeManager.lua

--- ThemeManager.lua
+++ ThemeManager.lua
@@ -50,7 +50,7 @@ local ThemeManager = {} do
 	end
 
 	function ThemeManager:LoadDefault()		
-		local theme = 'LockyStar'
+		local theme = 'Locky Star'
 		local content = isfile(self.Folder .. '/themes/default.txt') and readfile(self.Folder .. '/themes/default.txt')
 
 		local isDefault = true
